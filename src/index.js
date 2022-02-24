56b1545bc19ee37d7746ef06595f39f48d97135d
Jeff Anderson <60357576+medfieldanderson@users.noreply.github.com>
2022-02-24 15:11:55 -0500

    CORB prevent me from adding images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1>My Favourite Foods</h1>
+    <img alt="" src={img + "?grayscale"} />
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
